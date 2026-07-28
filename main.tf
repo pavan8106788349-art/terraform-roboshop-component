12f0b83e85ce36ee92d6fa6b7e41d63603a4d3b3
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-07-28 19:50:05 +0530

    hjk

--- main.tf
+++ main.tf
@@ -123,7 +123,7 @@ resource "aws_autoscaling_group" "main" {
   min_size                  = 1
   health_check_grace_period = 120
   health_check_type         = "ELB"
-  desired_capacity          = 1
+  desired_capacity          = 0
   force_delete              = false
 
   launch_template {
